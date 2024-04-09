4e143dfe0ded10b0b9ba79ebb58c88a8f7b30f81
Guillaume Batier <guillaume.batier@gmail.com>
2024-04-09 13:49:54 +0200

    Update src/Commands/Translations/ImportLanguage.php
    Co-authored-by: Touxten <156076965+Touxten@users.noreply.github.com>

--- src/Commands/Translations/ImportLanguage.php
+++ src/Commands/Translations/ImportLanguage.php
@@ -19,7 +19,7 @@ final class ImportLanguage extends Command
             ->setName('fop:translations:import')
             ->setDescription('Import a translation by iso code')
             ->setHelp('This command will import or update a translation package')
-            ->addUsage('./bin/console op:translations:import fr ( import fr translation package )')
+            ->addUsage('./bin/console fop:translations:import fr import fr translation package ')
             ->addArgument(
                 'iso',
                 InputOption::VALUE_REQUIRED,
